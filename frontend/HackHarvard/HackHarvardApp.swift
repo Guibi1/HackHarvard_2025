7491d3fcf023f2685ab68120bb620c2748a348c7
Laurent Stéphenne <laurent@guibi.dev>
2025-10-03 20:26:23 -0400

    First commit frontend

---
+++ frontend/HackHarvard/HackHarvardApp.swift
@@ -0,0 +1,20 @@
+//
+//  HackHarvardApp.swift
+//  HackHarvard
+//
+//  Created by Laurent Stéphenne on 2025-10-03.
+//
+
+import SwiftUI
+
+@main
+struct HackHarvardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+                ContentView()
+           
+        }
+    }
+}
+
